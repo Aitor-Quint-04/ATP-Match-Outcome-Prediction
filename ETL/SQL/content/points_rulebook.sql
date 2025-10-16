da80da66096438c26d974c4ce703fab011de1d1b
Aitor Quintanilla Reguero <aitor.quint.reg@gmail.com>
2025-10-16 19:47:42 +0200

    Create points_rulebook.sql

---
+++ ETL/SQL/content/points_rulebook.sql
@@ -0,0 +1,194 @@
+set feedback off
+set define off
+prompt Loading POINTS_RULEBOOK...
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'F', 'W', 2000);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'F', 'L', 1200);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'SF', 'L', 720);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'QF', 'L', 360);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'R16', 'L', 180);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'R32', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'R64', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'R128', 'L', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'Q3', 'W', 25);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'F', 'W', 1000);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'F', 'L', 600);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'SF', 'L', 360);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'QF', 'L', 180);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'R16', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'R32', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'R64', 'L', 25);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'R128', 'L', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'Q', 'W', 16);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'F', 'W', 1000);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'F', 'L', 600);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'SF', 'L', 360);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'QF', 'L', 180);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'R16', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'R32', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'R64', 'L', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'Q', 'W', 25);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'F', 'W', 500);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'F', 'L', 300);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'SF', 'L', 180);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'QF', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'R16', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'R32', 'L', 20);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'Q', 'W', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'Q', 'W', 20);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'R16', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'QF', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'SF', 'L', 180);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'F', 'L', 300);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'F', 'W', 500);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'F', 'W', 250);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'F', 'L', 150);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'SF', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'QF', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'R16', 'L', 20);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'R32', 'L', 5);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'Q', 'W', 5);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'Q', 'W', 12);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'R16', 'L', 20);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'QF', 'L', 45);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'SF', 'L', 90);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'F', 'L', 150);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'F', 'W', 250);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'F', 'W', 100);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'F', 'L', 60);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'SF', 'L', 35);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'QF', 'L', 18);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'R16', 'L', 8);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'R32', 'L', 5);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'Q', 'W', 4);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'F', 'W', 50);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'F', 'L', 30);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'SF', 'L', 17);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'QF', 'L', 9);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'R16', 'L', 4);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'Q', 'W', 2);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gsCup', 'R16', 'L', 75);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gsCup', 'SF', 'L', 150);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gsCup', 'QF', 'L', 300);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gsCup', 'F', 'L', 450);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gsCup', 'F', 'W', 600);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'Q3', 'L', 16);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('gs', 'Q2', 'L', 8);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R32', 'Q', 'L', 6);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R128', 'Q', 'L', 8);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('1000-R64', 'Q', 'L', 16);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R32', 'Q', 'L', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp500-R64', 'Q', 'L', 4);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atp250-R64', 'Q', 'L', 3);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch50', 'Q', 'L', 1);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('ch100', 'Q', 'L', 2);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atpFinals', 'RR', 'W', 200);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atpFinals', 'F', 'W', 900);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('atpFinals', 'F', 'L', 400);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu25', 'F', 'W', 20);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu25', 'F', 'L', 12);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu25', 'SF', 'L', 6);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu25', 'QF', 'L', 3);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu25', 'R16', 'L', 1);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu15', 'F', 'W', 10);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu15', 'F', 'L', 6);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu15', 'SF', 'L', 4);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu15', 'QF', 'L', 2);
+insert into POINTS_RULEBOOK (points_rule_id, stadie_id, result, points)
+values ('fu15', 'R16', 'L', 1);
+commit;
+prompt 93 records loaded
+set feedback on
+set define on
+prompt Done.
